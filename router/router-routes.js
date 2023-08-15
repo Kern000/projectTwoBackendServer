d140873e20396c7ff1b2f9b5f0cf8d855b88d19b
unknown <holupkern@gmail.com>
2023-08-15 11:30:36 +0800

    more minor formatting changes

--- router/router-routes.js
+++ router/router-routes.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const { 
-        updateProfile, 
+const { updateProfile, 
         addToNestedArray,
         deleteFromNestedArray,
         retrieveNestedArray,
         findItem,
       } = require("../controller-layer/entry-controller");
 
-const { 
-        login,
+const { login,
         register
       } = require("../controller-layer/user-controller");
 
 const { checkAccessAuthorization } = require("../middleware/uid-authorization")
 
-// Routes
-
-router.use("/:id", checkAccessAuthorization);  //all user related will check AccessAuthorization with token
+router.use("/:id", checkAccessAuthorization);
 
 router.get("/login", login);
 router.post("/register", register);
